3028f1c1ced8ffbb92585a3a70b4b8ded517b4f2
funayamateppei <t.funayama0217@gmail.com>
2023-01-17 12:42:31 +0900

    修正

--- services/omikuji.service.js
+++ services/omikuji.service.js
@@ -1,6 +1,6 @@
 export const getOmikuji = async (query) => {
   try {
-    const omikuji = ["大吉", "中吉", "小吉", "凶", "大凶"];
+    const omikuji = ["大吉", "吉", "中吉", "小吉", "凶", "大凶"];
     const min = 0;
     const max = omikuji.length - 1;
     const index = Math.floor(Math.random() * (max - min + 1)) + min;
