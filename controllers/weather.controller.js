7da456ad7902eeef13fcbc6224ebf1238ea4f03b
funayamateppei <t.funayama0217@gmail.com>
2023-01-24 13:02:59 +0900

    外部APIを使用してデータを取得する処理実装

--- controllers/weather.controller.js
+++ controllers/weather.controller.js
@@ -1,8 +1,13 @@
 import { getWeather } from "../services/weather.service.js";
 
 export const getResult = async (req, res, next) => {
+  // GETで送られてきたデータ(緯度経度)を確認する
+  console.log(req.query);
+  // 緯度経度を取り出す(送られてこなければデフォルトで東京の緯度経度を設定)
+  const latitude = Number(req.query.latitude ?? 35.6785);
+  const longitude = Number(req.query.longitude ?? 139.6823);
   try {
-    const result = await getWeather();
+    const result = await getWeather({latitude, longitude});
     return res.status(200).json({
       status: 200,
       data: result,
